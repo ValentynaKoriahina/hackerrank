d532b89d5b522da1475df68d4b2a9f7b7462ce91
Valentyna_Koriahina <v.koriagina.work@gmail.com>
2022-12-28 20:14:12 +0000

    initial commit

---
+++ itertools-combinations.py
@@ -0,0 +1,12 @@
+import itertools
+
+S, k = input().split()
+
+size_range = itertools.count(start=1, step=1);
+
+S = ''.join(sorted((list(S))))
+
+for _ in range(int(k)):
+    comb_S = list(itertools.combinations(S, next(size_range)))
+    for j in comb_S:
+     print(*j, sep='')
