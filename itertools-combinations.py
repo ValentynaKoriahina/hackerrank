992063b86f6537905d9725cf62bb23b246cf91dd
Valentyna_Koriahina <v.koriagina.work@gmail.com>
2022-12-28 21:10:29 +0000

    commit

--- itertools-combinations.py
+++ itertools-combinations.py
@@ -1,12 +1,16 @@
 import itertools
 
-S, k = input().split()
 
-size_range = itertools.count(start=1, step=1);
+def my_solution():
 
-S = ''.join(sorted((list(S))))
+    S, k = input().split()
+    size_range = itertools.count(start=1, step=1);
+    S = ''.join(sorted((list(S))))
 
-for _ in range(int(k)):
-    comb_S = list(itertools.combinations(S, next(size_range)))
-    for j in comb_S:
-     print(*j, sep='')
+    for _ in range(int(k)):
+        comb_S = list(itertools.combinations(S, next(size_range)))
+        for j in comb_S:
+            print(*j, sep='')
+
+if __name__ == '__main__':
+    my_solution()
