50847b1d3a224c433088eaa0df50a18ed49a7011
Valentyna_Koriahina <v.koriagina.work@gmail.com>
2023-01-02 11:41:20 +0000

    commit

--- venv/compress-the-string_2.py
+++
@@ -1,9 +0,0 @@
-# Sample Input: '1222311'
-# Sample Output: (1, 1) (3, 2) (1, 3) (2, 1)
-
-import itertools
-
-S = list(map(int, input()))
-
-for i, j in itertools.groupby(S):
-    print(tuple([len(list(j)), i]), end=' ')
